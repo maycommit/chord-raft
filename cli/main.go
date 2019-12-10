f007ca13d228fdc71ac7f0ce5281740ba043bb15
Maycon Pacheco <mayconjrpacheco@gmail.com>
2019-12-10 18:00:42 -0300

    Create storage commands in cli

--- cli/main.go
+++ cli/main.go
@@ -1,17 +1,43 @@
 package main
 
 import (
+	"context"
 	"fmt"
 	"log"
 	"os"
+	"strconv"
 	"time"
 
+	"sdproject/protos"
 	node "sdproject/src"
 
 	"github.com/urfave/cli"
+	"google.golang.org/grpc"
 )
 
 var app = cli.NewApp()
+
+func NewGrpcConn(remoteConn string) (protos.ChordClient, error) {
+
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+
+	dialOptions := []grpc.DialOption{
+		grpc.WithBlock(),
+		grpc.WithTimeout(10 * time.Second),
+		grpc.FailOnNonTempDialError(true),
+		grpc.WithInsecure(),
+	}
+
+	conn, err := grpc.DialContext(ctx, remoteConn, dialOptions...)
+	if err != nil {
+		panic(err)
+	}
+
+	client := protos.NewChordClient(conn)
+
+	return client, nil
+}
 
 func info() {
 	app.Name = "Project CLI"
@@ -24,8 +50,8 @@ func commands() {
 	app.Commands = []cli.Command{
 		{
 			Name:    "createChord",
-			Aliases: []string{"cd"},
-			Usage:   "Create chord (First node)",
+			Aliases: []string{"cd", "create"},
+			Usage:   "Create chord (string)",
 			Flags: []cli.Flag{
 				&cli.BoolFlag{Name: "watch, w"},
 			},
@@ -45,8 +71,8 @@ func commands() {
 		},
 		{
 			Name:    "joinNode",
-			Aliases: []string{"j"},
-			Usage:   "Join node in chord - (newNode, chordNode)",
+			Aliases: []string{"j", "join"},
+			Usage:   "Join node in chord - (string string)",
 			Flags: []cli.Flag{
 				&cli.BoolFlag{Name: "watch, w"},
 			},
@@ -62,6 +88,23 @@ func commands() {
 						fmt.Printf(node.String())
 					}
 				}
+			},
+		},
+		{
+			Name:    "storageGet",
+			Aliases: []string{"g", "get"},
+			Usage:   "Get data in node - (string int)",
+			Action: func(c *cli.Context) {
+				conn, err := NewGrpcConn(c.Args().First())
+				if err != nil {
+					fmt.Printf("Connection error: ", err.Error())
+					return
+				}
+
+				key, _ := strconv.Atoi(c.Args().Get(1))
+				value, _ := conn.StorageGetRPC(context.Background(), &protos.Key{Key: int64(key)})
+
+				fmt.Printf("VALUE = %s\n", value.Value)
 			},
 		},
 	}
